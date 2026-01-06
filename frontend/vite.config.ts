3785866e95a8fce547ceaf575e8dca9ebdf7eddd
soufiane redouane <soulf83@gmail.com>
2026-01-06 14:32:39 +0100

    render html

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,27 @@
-// frontend/vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import prerender from "@prerenderer/rollup-plugin";
+import PuppeteerRenderer from "@prerenderer/renderer-puppeteer";
+
+export default defineConfig(({ command }) => {
+  const isBuild = command === "build";
+
+  return {
+    plugins: [
+      react(),
+
+      
+      isBuild &&
+        prerender({
+          
+          routes: ["/", "/ai-cv-generator", "/cover-letter-generator", "/pricing"],
+
+          
+          renderer: new PuppeteerRenderer({
+            renderAfterTime: 1000,
+          }),
+        }),
+    ].filter(Boolean),
+  };
+});
